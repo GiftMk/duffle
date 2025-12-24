5ab01158aea2ddd836176b913eb06e05c80c0e78
Gift Mkwara <gift.mkwara@gmail.com>
2025-12-24 21:22:38 +1300

    format

--- packages/app/src/components/kanban-board.tsx
+++ packages/app/src/components/kanban-board.tsx
@@ -1,51 +1,51 @@
-import { DragDropContext, DropResult } from "@hello-pangea/dnd";
-import { eq, useLiveQuery } from "@tanstack/react-db";
-import { FC } from "react";
-import { boardCollection } from "@/state/collections";
-import { ColumnCard } from "./column-card";
-import { moveTasks } from "@/lib/dnd";
+import { DragDropContext, DropResult } from '@hello-pangea/dnd'
+import { eq, useLiveQuery } from '@tanstack/react-db'
+import { FC } from 'react'
+import { moveTasks } from '@/lib/dnd'
+import { boardCollection } from '@/state/collections'
+import { ColumnCard } from './column-card'
 
 type KanbanProps = {
-  id: string;
-};
+	id: string
+}
 
 const handleDragEnd = (result: DropResult) => {
-  if (!result.destination) {
-    return;
-  }
+	if (!result.destination) {
+		return
+	}
 
-  moveTasks({
-    source: {
-      columnId: result.source.droppableId,
-      index: result.source.index,
-    },
-    destination: {
-      columnId: result.destination.droppableId,
-      index: result.destination.index,
-    },
-  });
-};
+	moveTasks({
+		source: {
+			columnId: result.source.droppableId,
+			index: result.source.index,
+		},
+		destination: {
+			columnId: result.destination.droppableId,
+			index: result.destination.index,
+		},
+	})
+}
 
 export const KanbanBoard: FC<KanbanProps> = ({ id }) => {
-  const { data: board } = useLiveQuery((q) =>
-    q
-      .from({ board: boardCollection })
-      .where(({ board }) => eq(board.id, id))
-      .findOne(),
-  );
+	const { data: board } = useLiveQuery((q) =>
+		q
+			.from({ board: boardCollection })
+			.where(({ board }) => eq(board.id, id))
+			.findOne(),
+	)
 
-  if (!board) {
-    // TODO: handle this better, return some form of skeleton component that shows board not found
-    return null;
-  }
+	if (!board) {
+		// TODO: handle this better, return some form of skeleton component that shows board not found
+		return null
+	}
 
-  return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div className="w-full overflow-x-auto h-full items-start justify-center flex gap-2">
-        {board.columns.map((id) => (
-          <ColumnCard id={id} />
-        ))}
-      </div>
-    </DragDropContext>
-  );
-};
+	return (
+		<DragDropContext onDragEnd={handleDragEnd}>
+			<div className='w-full overflow-x-auto h-full items-start justify-center flex gap-2'>
+				{board.columns.map((id) => (
+					<ColumnCard id={id} />
+				))}
+			</div>
+		</DragDropContext>
+	)
+}
--- packages/app/src/lib/dnd.ts
+++ packages/app/src/lib/dnd.ts
@@ -1,35 +1,35 @@
-import { columnCollection } from "@/state/collections";
+import { columnCollection } from '@/state/collections'
 
 type DragEndTarget = {
-  columnId: string;
-  index: number;
-};
+	columnId: string
+	index: number
+}
 
 type DragEndProps = {
-  source: DragEndTarget;
-  destination: DragEndTarget;
-};
+	source: DragEndTarget
+	destination: DragEndTarget
+}
 
 export const moveTasks = ({ source, destination }: DragEndProps) => {
-  let sourceTaskId: string | undefined;
+	let sourceTaskId: string | undefined
 
-  columnCollection.update(source.columnId, (draft) => {
-    const [taskId] = draft.tasks.splice(source.index, 1);
-    sourceTaskId = taskId;
-  });
+	columnCollection.update(source.columnId, (draft) => {
+		const [taskId] = draft.tasks.splice(source.index, 1)
+		sourceTaskId = taskId
+	})
 
-  const targetColumnId =
-    source.columnId === destination.columnId
-      ? source.columnId
-      : destination.columnId;
+	const targetColumnId =
+		source.columnId === destination.columnId
+			? source.columnId
+			: destination.columnId
 
-  columnCollection.update(targetColumnId, (draft) => {
-    if (!sourceTaskId) {
-      throw new Error(
-        `Failed to move tasks. Task not found at source index ${source.index}`,
-      );
-    }
+	columnCollection.update(targetColumnId, (draft) => {
+		if (!sourceTaskId) {
+			throw new Error(
+				`Failed to move tasks. Task not found at source index ${source.index}`,
+			)
+		}
 
-    draft.tasks.splice(destination.index, 0, sourceTaskId);
-  });
-};
+		draft.tasks.splice(destination.index, 0, sourceTaskId)
+	})
+}
