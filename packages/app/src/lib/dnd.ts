0951540513bb1de766554e2f13f45ee81555e997
Gift Mkwara <gift.mkwara@gmail.com>
2025-12-24 21:22:28 +1300

    got dnd peristing

---
+++ packages/app/src/lib/dnd.ts
@@ -0,0 +1,35 @@
+import { columnCollection } from "@/state/collections";
+
+type DragEndTarget = {
+  columnId: string;
+  index: number;
+};
+
+type DragEndProps = {
+  source: DragEndTarget;
+  destination: DragEndTarget;
+};
+
+export const moveTasks = ({ source, destination }: DragEndProps) => {
+  let sourceTaskId: string | undefined;
+
+  columnCollection.update(source.columnId, (draft) => {
+    const [taskId] = draft.tasks.splice(source.index, 1);
+    sourceTaskId = taskId;
+  });
+
+  const targetColumnId =
+    source.columnId === destination.columnId
+      ? source.columnId
+      : destination.columnId;
+
+  columnCollection.update(targetColumnId, (draft) => {
+    if (!sourceTaskId) {
+      throw new Error(
+        `Failed to move tasks. Task not found at source index ${source.index}`,
+      );
+    }
+
+    draft.tasks.splice(destination.index, 0, sourceTaskId);
+  });
+};
