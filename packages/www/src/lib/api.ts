227a17039cbdeddc655a49f087d10e29b5e6e325
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-06 19:50:43 +1200

    PG lite persisting to local file path

---
+++ packages/www/src/lib/api.ts
@@ -0,0 +1,62 @@
+import type { App } from '@duffle/api'
+import { edenFetch } from '@elysia/eden/fetch'
+import { env } from '@/environment'
+
+export const fetch = edenFetch<App>(env.VITE_API_URL)
+
+export const createDocument = (
+	id: string,
+	markdown: string,
+	createdAt: string,
+) => {
+	return fetch('/api/documents/create', {
+		method: 'POST',
+		body: {
+			id,
+			markdown,
+			createdAt,
+		},
+	})
+}
+
+export const updateDocument = (
+	id: string,
+	markdown: string,
+	updatedAt: string,
+) => {
+	return fetch('/api/documents/update', {
+		method: 'PUT',
+		body: {
+			id,
+			markdown,
+			updatedAt,
+		},
+	})
+}
+
+export const getDocument = (id: string) => {
+	return fetch('/api/documents/:id', {
+		method: 'GET',
+		params: {
+			id,
+		},
+	})
+}
+
+export const getDocuments = (q?: string) => {
+	return fetch('/api/documents', {
+		method: 'GET',
+		query: {
+			q,
+		},
+	})
+}
+
+export const deleteDocument = (id: string) => {
+	return fetch('/api/documents/:id', {
+		method: 'DELETE',
+		params: {
+			id,
+		},
+	})
+}
