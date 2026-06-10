1a09f2b20747924ac8a6aad087bb389b4cf3a48d
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-10 20:28:22 +1200

    added defaults for port and database url env vars

--- packages/api/environment.ts
+++ packages/api/environment.ts
@@ -2,9 +2,9 @@ import { createEnv } from '@t3-oss/env-core'
 import z from 'zod'
 
 export const env = createEnv({
-	server: {
-		DATABASE_URL: z.string(),
-		PORT: z.number().default(3000),
-	},
-	runtimeEnv: process.env,
+  server: {
+    DATABASE_URL: z.string().default('.pglite'),
+    PORT: z.coerce.number().default(3000),
+  },
+  runtimeEnv: process.env,
 })
