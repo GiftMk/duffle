c8d55a16611413ebd7652f52068d999ec2c4e758
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-10 20:30:56 +1200

    editor now handles scroll on overflow

--- packages/www/src/components/editor.tsx
+++ packages/www/src/components/editor.tsx
@@ -1,17 +1,17 @@
 import type { Document } from '@duffle/api'
 import {
-	defaultValueCtx,
-	Editor,
-	editorViewOptionsCtx,
-	rootCtx,
+  defaultValueCtx,
+  Editor,
+  editorViewOptionsCtx,
+  rootCtx,
 } from '@milkdown/kit/core'
 import { clipboard } from '@milkdown/kit/plugin/clipboard'
 import { listener } from '@milkdown/kit/plugin/listener'
 import { commonmark } from '@milkdown/kit/preset/commonmark'
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react'
 import {
-	ProsemirrorAdapterProvider,
-	useNodeViewFactory,
+  ProsemirrorAdapterProvider,
+  useNodeViewFactory,
 } from '@prosemirror-adapter/react'
 import { useAutoSave } from '../hooks/use-auto-save'
 import { codeBlock } from '../plugins/code-block'
@@ -21,39 +21,39 @@ import { blockquote } from '@/plugins/blockquote'
 import { inlineCode } from '@/plugins/inline-code'
 
 const EditorContent = ({ document }: { document: Document }) => {
-	const nodeViewFactory = useNodeViewFactory()
+  const nodeViewFactory = useNodeViewFactory()
 
-	useEditor((root) => {
-		return Editor.make()
-			.config((ctx) => {
-				ctx.set(rootCtx, root)
-				ctx.set(defaultValueCtx, document.markdown)
-				ctx.update(editorViewOptionsCtx, (prev) => ({
-					...prev,
-					attributes: { autofocus: 'true' },
-				}))
-			})
-			.use(codeBlock)
-			.use(blockquote(nodeViewFactory))
-			.use(inlineCode(nodeViewFactory))
-			.use(listener)
-			.use(commonmark)
-			.use(clipboard)
-			.use(headingLevelIndicator)
-	})
-	useAutoSave(document.id)
+  useEditor((root) => {
+    return Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, root)
+        ctx.set(defaultValueCtx, document.markdown)
+        ctx.update(editorViewOptionsCtx, (prev) => ({
+          ...prev,
+          attributes: { autofocus: 'true' },
+        }))
+      })
+      .use(codeBlock)
+      .use(blockquote(nodeViewFactory))
+      .use(inlineCode(nodeViewFactory))
+      .use(listener)
+      .use(commonmark)
+      .use(clipboard)
+      .use(headingLevelIndicator)
+  })
+  useAutoSave(document.id)
 
-	return <Milkdown />
+  return <Milkdown />
 }
 
 export const MarkdownEditor = ({ document }: { document: Document }) => {
-	return (
-		<div className='h-full w-full max-w-[65ch] py-12'>
-			<MilkdownProvider>
-				<ProsemirrorAdapterProvider>
-					<EditorContent document={document} />
-				</ProsemirrorAdapterProvider>
-			</MilkdownProvider>
-		</div>
-	)
+  return (
+    <div className='h-full w-full overflow-y-auto max-w-[75ch] py-12'>
+      <MilkdownProvider>
+        <ProsemirrorAdapterProvider>
+          <EditorContent document={document} />
+        </ProsemirrorAdapterProvider>
+      </MilkdownProvider>
+    </div>
+  )
 }
