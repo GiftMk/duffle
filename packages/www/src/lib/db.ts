7d46e39c7bc3094877cb40da4578ee8abca646f7
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-15 23:18:46 +1200

    Added console debugs around indexed db creation

--- packages/www/src/lib/db.ts
+++ packages/www/src/lib/db.ts
@@ -16,10 +16,16 @@ export const database = () => {
 
 export const initDb = async () => {
 	if (dbAtom.get()) {
+		console.debug('Skipping in-memory db creation')
 		return
 	}
 
 	const db = createDb(`idb://${env.VITE_DB_URL}`)
+	console.debug('Created in-memory db')
+
 	await runMigrations(db)
+	console.debug('Ran migrations')
+
 	dbAtom.set(db)
+	console.debug('Initialized in-memory db')
 }
