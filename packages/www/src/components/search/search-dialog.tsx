0b0f4e4a462c1ddfd22eda7f04a3095c6ef08b02
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-22 11:13:35 +1200

    Gave the search dialog a reasonable max height

--- packages/www/src/components/search/search-dialog.tsx
+++ packages/www/src/components/search/search-dialog.tsx
@@ -39,8 +39,8 @@ export const SearchDialog = () => {
 				}
 			/>
 			<Dialog.Portal>
-				<Dialog.Popup className='fixed top-44 left-1/2 w-2xl -translate-x-1/2 rounded-md border border-surface-400 bg-surface-100 p-4 shadow-2xl shadow-surface-400/50 focus:outline-none'>
-					<span className='flex w-full items-center gap-3 p-2'>
+				<Dialog.Popup className='fixed top-44 left-1/2 w-2xl -translate-x-1/2 rounded-md border border-surface-400 bg-surface-100 py-4 shadow-2xl shadow-surface-400/50 focus:outline-none'>
+					<span className='flex w-full items-center gap-3 py-2 px-4'>
 						<InputSwitch mode={mode} setMode={setMode} />
 						<Input
 							value={query}
@@ -50,28 +50,23 @@ export const SearchDialog = () => {
 							placeholder={'Search, ask...'}
 						/>
 					</span>
-					<ul className='flex flex-col'>
+					<ul className='flex flex-col overflow-y-auto max-h-[min(432px,35svh)] px-4'>
 						{results.map((note) => (
 							<Fragment key={note.id}>
-								<li className=''>
-									<Link
-										to='/notes/$noteId'
-										params={{ noteId: note.id }}
-										onClick={() => setOpen(false)}
-										className='flex w-full flex-col gap-4 rounded-md px-4 pt-6 pb-3 hover:bg-surface-300/70'
-									>
-										<p
-											className={cn('font-bold text-lg', {
-												'opacity-30': note.title.length === 0,
-											})}
-										>
-											{note.title.length > 0 ? note.title : 'Untitled'}
-										</p>
-										<p className='line-clamp-2 text-typography-500 text-xs'>
-											{note.body}
-										</p>
-									</Link>
-								</li>
+								<SearchResult item={note} onClick={() => setOpen(false)} />
+								<hr className='my-2 text-surface-400' />
+							</Fragment>
+						))}
+						{/* temp */}
+						{results.map((note) => (
+							<Fragment key={note.id}>
+								<SearchResult item={note} onClick={() => setOpen(false)} />
+								<hr className='my-2 text-surface-400' />
+							</Fragment>
+						))}
+						{results.map((note) => (
+							<Fragment key={note.id}>
+								<SearchResult item={note} onClick={() => setOpen(false)} />
 								<hr className='my-2 text-surface-400' />
 							</Fragment>
 						))}
@@ -79,5 +74,32 @@ export const SearchDialog = () => {
 				</Dialog.Popup>
 			</Dialog.Portal>
 		</Dialog.Root>
+	)
+}
+
+type SearchResultProps = {
+	item: SearchItem
+	onClick?: () => void
+}
+
+const SearchResult = ({ item, onClick }: SearchResultProps) => {
+	return (
+		<li>
+			<Link
+				to='/notes/$noteId'
+				params={{ noteId: item.id }}
+				onClick={onClick}
+				className='flex w-full flex-col gap-4 rounded-md px-4 pt-6 pb-3 hover:bg-surface-300/70'
+			>
+				<p
+					className={cn('font-bold text-lg', {
+						'opacity-30': item.title.length === 0,
+					})}
+				>
+					{item.title.length > 0 ? item.title : 'Untitled'}
+				</p>
+				<p className='line-clamp-2 text-typography-500 text-xs'>{item.body}</p>
+			</Link>
+		</li>
 	)
 }
