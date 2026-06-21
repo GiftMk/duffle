120ab873665c1d634631b480fa00861111e0bf20
Gift Mkwara <gift.mkwara@gmail.com>
2026-06-22 11:39:56 +1200

    increased vertical padding in editor

--- packages/www/src/components/editor.tsx
+++ packages/www/src/components/editor.tsx
@@ -74,7 +74,7 @@ export const MarkdownEditor = ({ note }: { note: Note }) => {
 	return (
 		<div
 			key={note.id}
-			className='flex h-full w-full justify-center overflow-y-auto px-12 py-4'
+			className='flex h-full w-full justify-center overflow-y-auto px-12 py-9'
 		>
 			<div className='h-fit min-h-full w-full max-w-[70ch]'>
 				<MilkdownProvider>
