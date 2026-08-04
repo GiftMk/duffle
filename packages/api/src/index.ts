aefbca1752fd6adc464add3337088ce9e93c7ce3
gift.mkwara <gift.mkwara@phocassoftware.com>
2026-08-05 09:56:54 +1200

    vercel fix

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -30,9 +30,10 @@ const apiRoutes = new Hono<RequestEnv>()
 export const routes = app.route('/api', apiRoutes)
 export type AppType = typeof routes
 
-export const GET = handle(app)
-export const POST = handle(app)
-export const PUT = handle(app)
-export const DELETE = handle(app)
-export const PATCH = handle(app)
+const handler = handle(app)
+export const GET = (req: Request) => handler(req)
+export const POST = (req: Request) => handler(req)
+export const PUT = (req: Request) => handler(req)
+export const DELETE = (req: Request) => handler(req)
+export const PATCH = (req: Request) => handler(req)
 export default app
