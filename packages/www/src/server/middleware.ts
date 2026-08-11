9808bed3325a60339c05b065108a3b06100c2df5
Gift Mkwara <gift.mkwara@gmail.com>
2026-08-11 23:44:47 +1200

    dropped the www package

--- packages/www/src/server/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { createMiddleware } from '@tanstack/react-start'
-import { getDb } from '@/db'
-
-export const withDb = createMiddleware({ type: 'function' }).server(
-	async ({ next }) => {
-		const db = await getDb()
-		return next({ context: { db } })
-	},
-)
