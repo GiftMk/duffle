ca8349f714ca9005ad5b22a98a25ead7fb03f3bd
gift.mkwara <gift.mkwara@phocassoftware.com>
2026-08-23 01:21:45 +1200

    editor now has height 100%

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,9 +1,9 @@
 import {
-	defaultValueCtx,
-	Editor,
-	editorViewCtx,
-	editorViewOptionsCtx,
-	rootCtx,
+  defaultValueCtx,
+  Editor,
+  editorViewCtx,
+  editorViewOptionsCtx,
+  rootCtx,
 } from '@milkdown/kit/core'
 import { clipboard } from '@milkdown/kit/plugin/clipboard'
 import { listener, listenerCtx } from '@milkdown/kit/plugin/listener'
@@ -11,9 +11,9 @@ import { commonmark } from '@milkdown/kit/preset/commonmark'
 import { TextSelection } from '@milkdown/kit/prose/state'
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react'
 import {
-	ProsemirrorAdapterProvider,
-	useNodeViewFactory,
-	usePluginViewFactory,
+  ProsemirrorAdapterProvider,
+  useNodeViewFactory,
+  usePluginViewFactory,
 } from '@prosemirror-adapter/react'
 import './editor.css'
 import '@milkdown/kit/prose/view/style/prosemirror.css'
@@ -32,102 +32,102 @@ import { inlineCode } from '@/prosemirror/inline-code'
 import { listItem } from '@/prosemirror/list-item'
 
 type EditorContentProps = {
-	defaultValue: string
-	onChange: (markdown: string) => void
+  defaultValue: string
+  onChange: (markdown: string) => void
 }
 
 const EditorContent = ({ defaultValue, onChange }: EditorContentProps) => {
-	const nodeViewFactory = useNodeViewFactory()
+  const nodeViewFactory = useNodeViewFactory()
 
-	const { get, loading } = useEditor((root) => {
-		return (
-			Editor.make()
-				.config((ctx) => {
-					ctx.set(rootCtx, root)
-					ctx.set(defaultValueCtx, defaultValue)
-					// ctx.set(slash.key, {
-					// 	view: pluginViewFactory({
-					// 		component: SlashCommands,
-					// 	}),
-					// })
-					ctx.update(listenerCtx, (prev) => {
-						prev.markdownUpdated((_, markdown) => {
-							onChange(markdown)
-						})
-						return prev
-					})
-					ctx.update(editorViewOptionsCtx, (prev) => ({
-						...prev,
-						attributes: { autofocus: 'true' },
-					}))
-				})
-				.use(commonmark)
-				.use(gfm)
-				.use(codeBlock)
-				.use(blockquote(nodeViewFactory))
-				.use(inlineCode(nodeViewFactory))
-				.use(listItem(nodeViewFactory))
-				.use(imageBlock(nodeViewFactory))
-				// .use(slash)
-				.use(listener)
-				.use(clipboard)
-				.use(headingLevelIndicator)
-				.use(autoscroll)
-				.use(history)
-				.use(trailing)
-				.use(indent)
-		)
-	})
+  const { get, loading } = useEditor((root) => {
+    return (
+      Editor.make()
+        .config((ctx) => {
+          ctx.set(rootCtx, root)
+          ctx.set(defaultValueCtx, defaultValue)
+          // ctx.set(slash.key, {
+          // 	view: pluginViewFactory({
+          // 		component: SlashCommands,
+          // 	}),
+          // })
+          ctx.update(listenerCtx, (prev) => {
+            prev.markdownUpdated((_, markdown) => {
+              onChange(markdown)
+            })
+            return prev
+          })
+          ctx.update(editorViewOptionsCtx, (prev) => ({
+            ...prev,
+            attributes: { autofocus: 'true' },
+          }))
+        })
+        .use(commonmark)
+        .use(gfm)
+        .use(codeBlock)
+        .use(blockquote(nodeViewFactory))
+        .use(inlineCode(nodeViewFactory))
+        .use(listItem(nodeViewFactory))
+        .use(imageBlock(nodeViewFactory))
+        // .use(slash)
+        .use(listener)
+        .use(clipboard)
+        .use(headingLevelIndicator)
+        .use(autoscroll)
+        .use(history)
+        .use(trailing)
+        .use(indent)
+    )
+  })
 
-	const initRef = useRef(false)
+  const initRef = useRef(false)
 
-	useEffect(() => {
-		if (initRef.current || loading) return
+  useEffect(() => {
+    if (initRef.current || loading) return
 
-		const editor = get()
-		if (!editor) return
+    const editor = get()
+    if (!editor) return
 
-		initRef.current = true
-		editor.action((ctx) => {
-			const view = ctx.get(editorViewCtx)
-			const selection = TextSelection.atEnd(view.state.doc)
-			view.dispatch(view.state.tr.setSelection(selection))
-			view.focus()
-		})
-	}, [loading, get])
+    initRef.current = true
+    editor.action((ctx) => {
+      const view = ctx.get(editorViewCtx)
+      const selection = TextSelection.atEnd(view.state.doc)
+      view.dispatch(view.state.tr.setSelection(selection))
+      view.focus()
+    })
+  }, [loading, get])
 
-	return <Milkdown />
+  return <Milkdown />
 }
 
 type MarkdownEditorProps = {
-	defaultValue: string
-	onChange: (markdown: string) => void
-	className?: string
+  defaultValue: string
+  onChange: (markdown: string) => void
+  className?: string
 }
 
 export const MarkdownEditor = ({
-	defaultValue,
-	onChange,
-	className,
+  defaultValue,
+  onChange,
+  className,
 }: MarkdownEditorProps) => {
-	const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
 
-	return (
-		<div
-			ref={ref}
-			id='markdown-editor-container'
-			className={cn(
-				'scrollbar-gutter-stable flex h-[calc(100%-128px)] w-full justify-center overflow-y-auto px-12 pt-9',
-				className,
-			)}
-		>
-			<div className='h-full w-full max-w-[80ch]'>
-				<MilkdownProvider>
-					<ProsemirrorAdapterProvider>
-						<EditorContent defaultValue={defaultValue} onChange={onChange} />
-					</ProsemirrorAdapterProvider>
-				</MilkdownProvider>
-			</div>
-		</div>
-	)
+  return (
+    <div
+      ref={ref}
+      id='markdown-editor-container'
+      className={cn(
+        'scrollbar-gutter-stable flex h-full w-full justify-center overflow-y-auto px-12 pt-9',
+        className,
+      )}
+    >
+      <div className='h-full w-full max-w-[80ch]'>
+        <MilkdownProvider>
+          <ProsemirrorAdapterProvider>
+            <EditorContent defaultValue={defaultValue} onChange={onChange} />
+          </ProsemirrorAdapterProvider>
+        </MilkdownProvider>
+      </div>
+    </div>
+  )
 }
