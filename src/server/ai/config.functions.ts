cb8ec3dde80654456d58bde6499cc2fe6f69ea54
Gift Mkwara <gift.mkwara@gmail.com>
2026-08-25 20:15:32 +1200

    Dropped AI chat feature

--- src/server/ai/config.functions.ts
+++
@@ -1,6 +0,0 @@
-import { createServerFn } from '@tanstack/react-start'
-import { env } from '@/env'
-
-export const getAiChatEnabledFn = createServerFn({ method: 'GET' }).handler(
-	async () => Boolean(env.OPENAI_API_KEY),
-)
