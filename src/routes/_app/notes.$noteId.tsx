2918991c539c7b45ad8be36c9218d6aedca4a1c0
Gift Mkwara <gift.mkwara@gmail.com>
2026-08-28 20:43:51 +1200

    fixed router cache bug

--- src/routes/_app/notes.$noteId.tsx
+++ src/routes/_app/notes.$noteId.tsx
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { useQueryClient, useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { debounce } from 'es-toolkit'
 import { MarkdownEditor } from '@/components/editor'
@@ -16,17 +16,25 @@ const updateNote = debounce(updateNoteFn, 250)
 
 function RouteComponent() {
 	const { noteId } = Route.useParams()
+	const queryClient = useQueryClient()
 	const { data: note } = useSuspenseQuery(noteQueryOptions(noteId))
 
 	if (!note) {
 		return <NoteNotFound />
 	}
 
+	const handleChange = (markdown: string) => {
+		queryClient.setQueryData(noteQueryOptions(note.id).queryKey, (current) =>
+			current ? { ...current, markdown } : current,
+		)
+		updateNote({ data: { id: note.id, markdown } })
+	}
+
 	return (
 		<MarkdownEditor
 			key={note.id}
 			defaultValue={note.markdown}
-			onChange={(markdown) => updateNote({ data: { id: note.id, markdown } })}
+			onChange={handleChange}
 		/>
 	)
 }
